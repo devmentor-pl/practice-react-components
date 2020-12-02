3e49e21a557978b0ff355b0408d801e88ef9b0b5
Martin Gawlyta <martin.guide@gmail.com>
2020-12-02 19:14:11 +0100

    Solve task 01

--- 01/app.js
+++ 01/app.js
@@ -5,11 +5,29 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+    
     render() {
-        console.log('render');
-
+        console.log('render...');
         return <h1>{ this.state.counter }</h1>
     }
+    componentDidMount() {
+        console.log('componentDidMount...');
+        this.counterInterval = setInterval(() => {
+            const { counter } = this.state
+            this.setState({counter: counter + 1})
+            }, 5000)
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate...');
+        clearInterval(this.counterInterval);
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount...');
+    }
+    forceUpdate() {
+        console.log('forceUpdate...');
+    }
+
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
