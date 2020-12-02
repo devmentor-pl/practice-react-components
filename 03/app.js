ffd6ec9d85e99d585a7e0692b8d7406f590003ad
Martin Gawlyta <martin.guide@gmail.com>
2020-12-03 00:22:53 +0100

    Solve task 03

--- 03/app.js
+++ 03/app.js
@@ -3,22 +3,57 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
-        comments: [],
+        comments: [
+            {
+                text: 'some comment 1',
+                id: 1
+            }, 
+            {
+                text: 'some comment 2',
+                id: 2
+            }, 
+        ],
+        commentInput: ''
     }
     
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({
+            comments: this.addComment(),
+            commentInput: ''
+        })
+    }
+    addComment = () => {
+        return this.state.comments.concat({
+            text: this.state.commentInput,
+            id: this.getId()
+        })
+    }
+    getId = () => this.state.comments.reduce((acc, comment) => acc = acc > comment.id ? acc : comment.id) + 1;
+
+    handleChange = (event) => {
+        event.preventDefault();
+        const commentText = event.target.value;
+        this.setState({ commentInput: commentText })
+    }
+
     render() {
         const {title, body} = this.props;
+        const { text, id } = this.state.comments;
+
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={ this.handleSubmit }>
                         <div>
                             <label>
                                 <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
                                     name="content" 
+                                    value={ this.state.commentInput }
+                                    onChange={ this.handleChange }
                                 />
                             </label>
                         </div>
@@ -26,6 +61,9 @@ class Article extends React.Component {
                     </form>
                     <ul>
                         {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.state.comments.map(comment => {
+                            return <li key={comment.id}>{comment.text}</li>
+                        })}
                     </ul>
                 </section>
             </article>
