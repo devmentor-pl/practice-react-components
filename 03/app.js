ac2d6f11070ccf16b224444ce2296f162504345a
Martyna Kiljan <kmartynak@icloud.com>
2020-12-18 21:24:06 +0100

    do sprawdzenia

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 class Article extends React.Component {
+   
+
+    /*nie wiem czy coś jest nie tak, bo  napisałeś, że mam wyczyścić textarea, ale on sam się czyści*/
+   
+  
     state = {
         comments: [],
+        textarea: "",
     }
+            handleCommentary = (event) => {
+                this.setState({
+                    textarea: event.target.value,
+                })
+                console.log(event.target.value)
+            
+
+            }
+
+            handleSubmit = (event) => {
+              
+                event.preventDefault();
+                const{textarea} = this.state;
+                this.addCommentary(`${textarea}`) 
+                this.setState({
+                        textarea: "",
+                })
+            }
+
+            addCommentary(comments) {
+                this.setState({
+                    comments: [...this.state.comments, comments]
+                })
+            }
+
+
+            addLi()  {
+              const {comments} = this.state;
+              const list =  comments.map((li,id) => {
+                  return <li key={id}> {li} </li>
+              })
+
+              return <>{list}</>
+        
+            }
+
     
+
+
     render() {
         const {title, body} = this.props;
         return (
@@ -13,19 +58,23 @@ class Article extends React.Component {
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.handleSubmit} >
                         <div>
                             <label>
                                 <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
+                                    name="content"
+                                    onChange={this.handleCommentary}
+                                    value ={this.state.textarea}
+                               
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                       {this.addLi()}
+                      
                     </ul>
                 </section>
             </article>
--- 04/app.js
+++ 04/app.js
@@ -6,19 +6,47 @@ class App extends React.Component {
         firstName: '',
         lastName: '',
         searchQuery: '',
-        users: ['Jan Kowalski', 'Michał Nowak'],
+        users: ['Jan Kowalski', 'Michał Nowak', 'Jan Grzech', 'Kazimierz Złoty'],
+    }
+ 
+
+    renderUsersList(name) {
+
+       if(this.state.searchQuery === '') {
+     
+            const {users} = this.state;
+            return users.map((name, id) => {
+                return (
+                    <li key={id} onClick={ this.clickHandler }>
+                        { name }
+                    </li>
+                );
+            });
+            
+       } else if (this.state.searchQuery.length > 0){
+    
+
+        const {users, searchQuery} = this.state;
+        return users.map((user,id) => {
+               if(user.toLowerCase().includes(searchQuery.toLowerCase())) {
+                   return (
+                       <li key={id} onClick={this.clickHandler}>
+                       { user }
+                       </li>
+                   )
+               }
+            })
+
+
+        }
+     
     }
 
-    renderUsersList() {
-        const {users} = this.state;
-        return users.map(name => {
-            return (
-                <li onClick={ this.clickHandler }>
-                    { name }
-                </li>
-            );
-        });
-    }
+
+ 
+
+      
+    
 
     clickHandler = e => {
         const {innerText: userName} = e.target;
@@ -39,14 +67,28 @@ class App extends React.Component {
                 <form>
                     <input name="firstName"
                         value={ firstName }
+                         placeholder="Dodaj Imię "
                         onChange={ this.inputChange }
                     />
                     <input name="lastName"
                         value={ lastName }
+                         placeholder="Nazwisko"
                         onChange={ this.inputChange }
                     />
                     <input type="submit"/>
                 </form>
+
+
+                <input
+                type="text"
+                value={this.searchQuery}
+                onChange={this.inputChange}
+                name="searchQuery"
+                placeholder="Wyszukaj"
+                
+                />
+
+
                 <ul>{ this.renderUsersList() }</ul>
             </section>
         );
