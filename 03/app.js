1b34398289e743e1a82ad2527e46a5f3905fe331
gregor232 <ktotok@gmail.com>
2021-02-08 18:12:01 +0100

    task 03 done

--- 03/app.js
+++ 03/app.js
@@ -2,30 +2,65 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
-    state = {
-        comments: [],
+    constructor(props) {
+        super(props)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.createList = this.createList.bind(this);
+        this.state = {
+            comments: ["komentarz"],
+            textValue: ""
+        }
     }
-    
+    handleSubmit(event) {
+
+        event.preventDefault();
+
+        if (this.state.textValue !== "") {
+            this.state.comments.push(this.state.textValue);
+            console.log(this.state.comments)
+            this.setState({textValue:""});
+        }
+
+    }
+    handleChange(event) {
+
+        this.setState({textValue: event.currentTarget.value});
+
+        
+    }
+    createList() {
+        const {comments} = this.state;
+
+        return comments.map((com) => {
+             return (
+                 <li>{com}</li>
+             )
+         })
+     }
+
     render() {
         const {title, body} = this.props;
+        
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.handleSubmit} >
                         <div>
                             <label>
                                 <textarea 
                                     style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                    name="content"
+                                    onChange={this.handleChange} 
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.createList()}
                     </ul>
                 </section>
             </article>
