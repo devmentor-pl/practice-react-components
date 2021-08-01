8bd3539aae4cf2728ef87e36a3db778618242b19
Laura Zacharuk <laura.zacharuk@gmail.com>
2021-08-01 17:35:16 +0200

    done task 1

--- 01/app.js
+++ 01/app.js
@@ -1,14 +1,52 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// class App extends React.Component {
+//     state = {
+//         counter: 0,
+//     }
+//     render() {
+//         console.log('render');
+
+//         return <h1>{ this.state.counter }</h1>
+//     }
+// }
+
+// ReactDOM.render(<App/>, document.querySelector('#root'));
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 }
     }
+
+   
     render() {
         console.log('render');
 
         return <h1>{ this.state.counter }</h1>
+    }
+    
+    componentDidMount() {
+        // console.log('Komponent został zmontowany')
+        this.id = setInterval(() => {
+            const {counter} = this.state;
+            this.setState({ counter: counter + 1 })
+        }, 5000);
+    }
+    componentDidUpdate() {
+        console.log('komponent został zamontowany');
+    }
+
+    componentWillUnmount() {
+        console.log('Komunikat po aktualizacji')
+        clearInterval(this.id);
     }
 }
 
