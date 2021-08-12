b27ad7e1d528f28537f9b4d14e1a937867e0a7f9
Radosław Kawczyński <kawczynski.radoslaw@gmail.com>
2021-08-12 13:06:45 +0200

    Task 01
    Task 01

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import counter from './components/styles/counter.js';
+
 class App extends React.Component {
-    state = {
-        counter: 0,
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 1,
+        }
     }
+
     render() {
         console.log('render');
-
-        return <h1>{ this.state.counter }</h1>
+        console.log(this.state);
+        return <h1 style={counter}>{this.state.counter}</h1>
     }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+
+        this.id = setInterval(() => {
+            let {counter} = this.state;
+            this.setState({counter: counter + 1});
+            console.log(counter + 1);
+            if ((counter + 1) === 10) {
+                clearInterval(this.id);
+                console.log('componentWillUnmount');
+            }
+        }, 5000);
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
