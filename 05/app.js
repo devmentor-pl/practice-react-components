33818000a31258f560c8e29306c7e5f6c640e750
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-08-25 17:13:04 +0100

    05

---
+++ 05/app.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+        };
+    }
+
+    componentDidMount() {
+        const url = `https://api.weatherbit.io/v2.0/current?key=a0d63ece07e944e18ce685e9a119e6bd&lat=52.232222&lon=21.008333&lang=pl`;
+        const promise = fetch(url);
+
+        return promise
+            .then((resp) => {
+                if (resp.ok) {
+                    return resp.json();
+                }
+                return Promise.reject(resp);
+            })
+            .then((d) => {
+                const weather = {
+                    city: d.data[0].city_name,
+                    conditions: d.data[0].weather.description,
+                    temp: d.data[0].app_temp,
+                };
+                this.setState({ data: weather });
+                return weather;
+            })
+            .catch((err) => console.log(err));
+    }
+
+    render() {
+        const { data } = this.state;
+        if (data) {
+            // renderuj dopiero jak pobierzesz dane z API
+
+            return (
+                <>
+                    <h1>informacje o pogodzie...</h1>
+                    <p>
+                        W miescie {this.state.data.city} jest obecnie{" "}
+                        {this.state.data.conditions.toLowerCase()}, a
+                        temperatura wynosi {this.state.data.temp}
+                    </p>
+                </>
+            );
+        }
+
+        // nic nie renderuj
+        return null;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
