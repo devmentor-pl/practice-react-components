5911a2fd0fda2be33586602eea4b9ef5d67855fa
Maciej Zadylak <maciej.zadylak@gmail.com>
2021-09-07 14:41:57 +0200

    Task 01

--- 01/app.js
+++ 01/app.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        }
     }
-    render() {
-        console.log('render');
 
+    render() {
+        
         return <h1>{ this.state.counter }</h1>
+    }
+
+    componentDidMount() {
+        console.log('Komponent został zamontowany');
+        this.id = setInterval(() => {
+            const { counter } = this.state;
+            this.setState({
+                counter: counter + 1,
+            })
+        }, 100)
+    }
+
+    componentDidUpdate() {
+        console.log('Aktualizacja komponentu!');
+    }
+
+    componentWillUnmount() {
+        console.log('Komponent został odmontowany');
+        clearInterval( this.id )
     }
 }
 
