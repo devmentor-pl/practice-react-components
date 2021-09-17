cb2f1c54cc828ba80a6d1423c140037957b91244
Kowacka Malgorzata <kowackag@gmail.com>
2021-09-17 09:28:32 +0200

    Finish task 05

---
+++ 05/app.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+
+class Weather extends React.Component {
+    state = {
+        data: null,
+    }
+
+    render() {
+        const {data} = this.state;
+        if (data) {
+            const {city_name, weather, temp} = this.state.data
+            return <h1>{`Pogoda w miejscowości ${city_name}: ${weather.description}, temperatura: ${temp} st. C`}</h1>
+        } return null
+    }
+
+    componentDidMount() {
+        const {lat, lng} = this.props;
+        const key = 'a597a49aa2bb4094bab1c0777c07229d';
+        const url = `https://api.weatherbit.io/v2.0/current?key=${key}&lat=${lat}&lon=${lng}&lang=pl`;
+        const promise = fetch(url);
+        promise
+            .then(resp =>{
+                if(resp.ok){
+                    return resp.json();
+                } if (resp.status === 400) {
+                    return Promise.reject('NIEPRAWIDŁOWE ŻĄDANIE');
+                }
+                return Promise.reject(resp);
+            })
+            .then(data => this.setData(data.data[0]))
+            .catch(err => console.log(err))
+    }
+
+    setData = (data) => {
+        this.setState({data: data});
+    }
+}
+
+
+
+ReactDOM.render(
+    <Weather lat ={52.2322222} lng = {21.008333}/>,
+    document.querySelector('#root')
+)
