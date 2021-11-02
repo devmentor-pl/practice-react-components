b7e82d2fa81b3d0c5f6ee10e67f66c043e470086
Jakub Chrobak <jchrobakk@gmail.com>
2021-11-02 14:04:10 +0100

    Finish task 02

--- 02/app.js
+++ 02/app.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class Counter extends React.Component {
-    state = {
-        amount: 0,
-    }
-    
-    render() {
-        return <button>click me ({ this.state.amount })</button>
-    }
+  state = {
+    amount: 0,
+  };
+
+  render() {
+    return (
+      <button onClick={this.clickHandler}>
+        click me ({this.state.amount})
+      </button>
+    );
+  }
+
+  clickHandler = () => {
+    this.setState({ amount: this.state.amount + 1 });
+  };
 }
 
-ReactDOM.render(<Counter />, document.querySelector('#root'));
+ReactDOM.render(<Counter />, document.querySelector("#root"));
