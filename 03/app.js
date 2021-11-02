42c42c3c3b4a8175cda8d45763adedbb2aba4762
Jakub Chrobak <jchrobakk@gmail.com>
2021-11-02 14:25:00 +0100

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -1,42 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class Article extends React.Component {
-    state = {
-        comments: [],
+  state = {
+    textarea: "",
+    comments: [],
+  };
+
+  render() {
+    const { title, body } = this.props;
+    const { textarea } = this.state;
+    return (
+      <article>
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <section>
+          <form onSubmit={this.submitHandler}>
+            <div>
+              <label>
+                <textarea
+                  onChange={this.changeHandler}
+                  style={{ minWidth: "300px", minHeight: "120px" }}
+                  name="content"
+                  value={textarea}
+                />
+              </label>
+            </div>
+            <div>
+              <input type="submit" value="dodaj komentarz" />
+            </div>
+          </form>
+          <ul>{this.renderCommentsList()}</ul>
+        </section>
+      </article>
+    );
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      textarea: e.target.value,
+    });
+  };
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    const { textarea } = this.state;
+    if (textarea) {
+      this.addComment(textarea);
+      this.setState({ textarea: "" });
+    } else {
+      alert("blank comment");
     }
-    
-    render() {
-        const {title, body} = this.props;
-        return (
-            <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-                <section>
-                    <form>
-                        <div>
-                            <label>
-                                <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
-                                />
-                            </label>
-                        </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
-                    </form>
-                    <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
-                    </ul>
-                </section>
-            </article>
-        )
-    }
+  };
+
+  addComment(textarea) {
+    this.setState({ comments: [...this.state.comments, textarea] });
+  }
+
+  renderCommentsList() {
+    const { comments } = this.state;
+    return comments.map((comment) => {
+      return <li>{comment}</li>;
+    });
+  }
 }
 
 ReactDOM.render(
-    <Article 
-        title="Programowanie jest super!"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />, 
-    document.querySelector('#root')
+  <Article
+    title="Programowanie jest super!"
+    body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
+  />,
+  document.querySelector("#root")
 );
