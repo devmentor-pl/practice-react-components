c2bdb86a841d259aff2ddb452cf2a0f15e5cc98e
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-19 22:50:27 +0100

    closes task 3

--- 03/app.js
+++ 03/app.js
@@ -4,39 +4,80 @@ import ReactDOM from 'react-dom';
 class Article extends React.Component {
     state = {
         comments: [],
-    }
-    
+        content: '',
+    };
+
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
+        const { content } = this.state;
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    style={{ minWidth: '300px', minHeight: '120px' }}
+                                    name="content"
+                                    value={content}
+                                    onChange={this.changeHandler}
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
-        )
-    }
+        );
+    };
+
+    renderCommentsList() {
+        const { comments } = this.state;
+        const doesCommentsExist = this.isArrayNotEmpty(comments);
+        if (!doesCommentsExist) {
+            return (
+                <li style={{ fontStyle: 'italic' }}>No comments</li>
+            );
+        };
+        return comments.map((comment, index) => <li key={index}>{comment}</li>);
+    };
+
+    isArrayNotEmpty(array) {
+        return array.length !== 0;
+    };
+
+    changeHandler = e => {
+        const { value } = e.target;
+        this.setState({ content: value });
+    };
+
+    submitHandler = e => {
+        e.preventDefault();
+        const { content } = this.state;
+        if (content.trim()) {
+            this.addComment(content);
+        } else {
+            alert('Comment can not be empty');
+        };
+    };
+
+    addComment(content) {
+        this.setState({
+            comments: [...this.state.comments, content],
+            content: ''
+        });
+    };
 }
 
 ReactDOM.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />, 
+    />,
     document.querySelector('#root')
 );
