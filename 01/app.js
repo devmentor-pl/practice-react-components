4aa9d37a950814fc06e7edb121d536ba0c832ea2
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-11-19 21:55:59 +0100

    closes task 1

--- 01/app.js
+++ 01/app.js
@@ -4,12 +4,32 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     state = {
         counter: 0,
-    }
+    };
+
     render() {
         console.log('render');
 
-        return <h1>{ this.state.counter }</h1>
-    }
+        return <h1>{this.state.counter}</h1>
+    };
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.id = setInterval(this.changeCounter, 5000);
+    };
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    };
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+        clearInterval(this.id);
+    };
+
+    changeCounter = () => {
+        const { counter } = this.state;
+        this.setState({ counter: counter + 1 });
+    };
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
