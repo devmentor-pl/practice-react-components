fdb8a5ab4f0850660a29cf5c920195ad24b40365
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-11-29 22:54:24 +0100

    Task 05 - done

--- 05/Api.js
+++ 05/Api.js
@@ -1,6 +1,6 @@
 export default class Api {
     constructor(lat , lon) {
-        this.key = '533a35b06f064afb9e648cca5cbb5099'
+        this.key = '038e0c12103f48d4994ef3e2eb391ee2'
         this.lat = lat;
         this.lon = lon;
         this.lang = 'pl'
@@ -9,19 +9,18 @@ export default class Api {
 
 
     async getWeatherData() {
-        return this._fetch(this.url)
+        return this._fetch(this.url);
     }
-
 
     _fetch(url) {
         return fetch(url)
             .then( resp => {
                 if(resp.ok) { return resp.json(); }
                 if (resp.status === 400) { return Promise.reject('Incorrect coordiates') }
+                if (resp.status === 403) { return Promise.reject('API key is incorrect') }
                 return Promise.reject(resp)
             })
-            .then ( data => { return data } )
-            .catch ( err => { throw new Error(err) } )
+            .catch ( err => console.log(err) )
     }
 }
 
