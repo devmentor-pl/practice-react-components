97020eca7079f2b245fba2387756a57e2fbc9fc5
Dawid Krawczyk <dawidosss1998@gmail.com>
2021-12-07 09:50:26 +0100

    First task

--- 01/app.js
+++ 01/app.js
@@ -10,6 +10,25 @@ class App extends React.Component {
 
         return <h1>{ this.state.counter }</h1>
     }
+
+    componentDidMount() {
+
+        this.id = setInterval( 
+           () => this.setState(
+               {counter: this.state.counter +1}
+           ), 5000);
+
+           console.log('Component mounted')
+    }
+
+    componentDidUpdate() {
+        console.log('Component did update');
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.id);
+        console.log('Component unmounted');
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
