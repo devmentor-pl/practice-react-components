97e5a62a2650ec81ecb43abf460adb775a59ea18
Rafał Kazik <kazik.raf@gmail.com>
2022-01-16 20:01:21 +0100

    webpack config, 1st task

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    state = {
-        counter: 0,
-    }
-    render() {
-        console.log('render');
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
 
-        return <h1>{ this.state.counter }</h1>
-    }
+  state = {
+    counter: 0,
+  };
+
+  render() {
+    console.log("render");
+
+    return <h1>{this.state.counter}</h1>;
+  }
+
+  componentDidMount() {
+    this.id = setInterval(() => {
+      this.setState({
+        counter: this.state.counter + 1,
+      });
+    }, 5000);
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.id);
+    console.log("componentWillUnmount");
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
