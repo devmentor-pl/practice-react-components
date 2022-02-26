69cd1d71a541c18175533235beb71f396d3c8f09
Ewelina <ewelina@ekopacz.pl>
2022-02-26 23:42:34 +0100

    add part of solution

--- 03/app.js
+++ 03/app.js
@@ -3,29 +3,52 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
-        comments: [],
+        comments: [''],
     }
-    
+
+    // z tym zadaniem mam problem, po wpisywaniu komentarza, od razu renderuje mi to co wpisuje na ekran
+    // w f-cji changeHandler pobieram wpisana zawartosc i aktualizuje setState i mam render
+    // nie wiem jak to zrobic aby dopiero po kliknicu w button pokazywalo komentarze
+
+    renderCommentsList() {
+        const {comments} = this.state;
+        return (comments.map(item => {
+            return(
+                <li>{item}</li>
+                )
+            })
+        )
+    }
+
+    changeHandler = e => { // aktualizuje wartosc w state.comments
+        this.setState({comments:[e.target.value]})
+    }
+
+    submmitHandler = e => {
+        e.preventDefault();
+    }
+
     render() {
         const {title, body} = this.props;
+        const {comments} = this.state;
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submmitHandler}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    style={{ "min-width": "300px", "min-height": "120px" }}
+                                    name="content" value={comments} onChange={this.changeHandler}
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
@@ -34,9 +57,9 @@ class Article extends React.Component {
 }
 
 ReactDOM.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />, 
+    />,
     document.querySelector('#root')
 );
