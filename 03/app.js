6c039197ea15d11eb4f508f20083444925c47755
Jakub Parol <kubciop33@gmail.com>
2022-05-14 16:14:52 +0200

    Finished exercise 3

--- 03/app.js
+++ 03/app.js
@@ -3,22 +3,24 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
+        comment: '',
         comments: [],
     }
     
     render() {
         const {title, body} = this.props;
+        const {comment} = this.state;
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
                                 <textarea 
                                     style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                    name="content" value={comment} onChange={this.commentChange}
                                 />
                             </label>
                         </div>
@@ -26,11 +28,43 @@ class Article extends React.Component {
                     </form>
                     <ul>
                         {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
         )
     }
+
+    renderCommentsList() {
+        const {comments} = this.state;
+        return comments.map(comment => <li>{comment}</li>);
+    }
+
+    addComment(value) {
+        this.setState({
+            comments: [...this.state.comments, value],
+        });
+    }
+
+    commentChange = e => {
+        this.setState({
+            comment: e.target.value,
+        })
+    }
+
+    submitHandler = e => {
+        e.preventDefault();
+        const {comment} = this.state;
+        if(comment) {
+            this.addComment(`${comment}`);
+            this.setState({
+                comment: '',
+            });
+        } else {
+            alert('Wpisz komentarz!');
+        }
+    }
+
 }
 
 ReactDOM.render(
