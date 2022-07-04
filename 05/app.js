9fd7785b7ac43698b13247eb4587229a8647a983
Andrzej <andis_as@wp.pl>
2022-07-04 11:45:14 +0200

    zad 5 - fetch weather data & set it in state

--- 05/app.js
+++ 05/app.js
@@ -2,50 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Weather extends React.Component {
-    state = {  } 
+    state = {
+        data: null
+    }
 
-    fetchData() {
-        return fetch('https://randomuser.me/api?results=2')
-        .then(res => {
-            console.log(res)
-            if(res.ok) {
-                return res.json()
-            }
-            return Promise.reject('Error')
-        })       
+    fetchData(URL) {
+        return fetch(URL)
+            .then(res => {
+                console.log(res)
+                if (res.ok) {
+                    return res.json()
+                }
+                return Promise.reject('Error')
+            })
     }
     componentDidMount() {
         console.log('componentDidMount')
         console.log(this.props)
-        const {lat, lng} = this.props
-        console.log(lat +' '+ lng)
+        const { lat, lng } = this.props
+        console.log(lat + ' ' + lng)
 
         const KEY = process.env.REACT_KEY
         console.log(KEY)
         const URL = `https://api.weatherbit.io/v2.0/current?key=${KEY}&lang=pl&lat=${lat}&lon=${lng}&units=M`
-        
-        const getData = this.fetchData()
+
+        const getData = this.fetchData(URL)
         getData
-            .then(data => console.log(data))
-            .catch(err => console.log(err.message)) 
+            .then(data => {
+                console.log(data)
+                this.setState({ data: data.data });
+            })
+            .catch(err => console.log(err.message))
 
     }
-    render() { 
+    render() {
         console.log(this.props)
-        return (
-            <h1>Weather</h1>
-        );
+        const { data } = this.state
+        if (data) {
+            return (
+                <div>
+                    <h1>Weather</h1>
+                    {
+
+                    }
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <h1>No data</h1>
+                </div>
+            )
+
+        }
+
     }
 }
 
 const App = () => {
-    return ( 
+    return (
         <div>
             <Weather lat={52.232222} lng={21.008333} />
         </div>
-     );
+    );
 }
- 
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
