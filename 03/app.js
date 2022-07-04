f3fc20b3ed193c023187a57bbc08d45f5090c235
Andrzej <andis_as@wp.pl>
2022-07-04 11:33:18 +0200

    zad 3 - done

--- 03/app.js
+++ 03/app.js
@@ -3,22 +3,38 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
+        value: '',
         comments: [],
     }
-    
+    changeHandler = e => {
+        const value = e.target.value 
+        this.setState({ value });
+    }
+    submitHandle = e => {
+        e.preventDefault()
+        if(this.state.value) {
+            this.setState({ 
+                comments: [...this.state.comments, this.state.value],
+                value: ''
+            });
+        }
+    }
     render() {
         const {title, body} = this.props;
+        const {comments} = this.state
+        console.log(comments)
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandle}>
                         <div>
                             <label>
                                 <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
+                                    name="content" onChange={this.changeHandler}
+                                    value={this.state.value}
                                 />
                             </label>
                         </div>
@@ -26,6 +42,11 @@ class Article extends React.Component {
                     </form>
                     <ul>
                         {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {
+                            comments.map((comment, index) => (
+                                <li key={index}>{comment}</li>
+                            ))
+                        }
                     </ul>
                 </section>
             </article>
