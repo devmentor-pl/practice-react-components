dfa22e92d80dc6fbcd68f4ccb3ee5bf8ce3b114a
Camila Montes Celinski <camicelinski@gmail.com>
2022-08-13 16:05:21 +0200

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-    state = {
-        counter: 0,
-    }
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 0,
+        }
+    }    
+
     render() {
-        console.log('render');
+        console.log('render')
 
         return <h1>{ this.state.counter }</h1>
     }
+
+    componentDidMount() {
+        console.log('component did mount')
+
+        this.id = setInterval(() => {
+            const {counter} = this.state
+            this.setState({ counter: counter + 1 })
+        }, 5000)
+    }
+
+    componentDidUpdate() {
+        console.log('component did update')
+    }
+
+    componentWillUnmount() {
+        console.log('component will unmount')
+
+        clearInterval( this.id )
+    }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App/>, document.querySelector('#root'))
