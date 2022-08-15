6f72b475b78acb1dba280fa26f31c6ccb6981897
Mateusz Suplewski <matx3582@gmail.com>
2022-08-15 21:08:57 +0200

    Task-3 Done

--- 03/app.js
+++ 03/app.js
@@ -3,33 +3,58 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
+        content: 'Empty comment',
         comments: [],
     }
     
     render() {
         const {title, body} = this.props;
+        const {content} = this.state
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler} >
                         <div>
                             <label>
                                 <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
+                                    name="content" value={content} onChange={this.textareaChange}
                                 />
                             </label>
                         </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
+                        <div><input type="submit" value="dodaj komentarz"/></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderComments()}
                     </ul>
                 </section>
             </article>
         )
+    }
+
+    renderComments(){
+        const {comments} = this.state
+        const commentsJSX = comments.map((comment,index) => <li key={index}>{comment}</li>)
+        return commentsJSX
+    }
+
+    textareaChange = (e) => {
+        const {value,name} = e.target
+        this.setState({[name]: value})
+    }
+
+    addComment = (comment) => {
+        const {comments} = this.state
+        this.setState({comments: [...comments,comment]})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        const {content} = this.state
+        this.addComment(content)
+        this.setState({content: ''})
     }
 }
 
