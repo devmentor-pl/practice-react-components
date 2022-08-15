b578683da9e87882f57e6670bd202daed07d9200
Mateusz Suplewski <matx3582@gmail.com>
2022-08-15 20:32:55 +0200

    Task-1 Done

--- 01/app.js
+++ 01/app.js
@@ -5,10 +5,32 @@ class App extends React.Component {
     state = {
         counter: 0,
     }
+
+    constructor(props){
+        console.log('constructor')
+        super(props)
+    }
+
     render() {
         console.log('render');
-
         return <h1>{ this.state.counter }</h1>
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+        const {counter} = this.state
+        this.id = setInterval(() => {
+            this.setState({ counter: counter + 1})
+        },5000)
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+        clearInterval(this.id)
     }
 }
 
