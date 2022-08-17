fdafbf7ca93a2ee22ad9d84fc7efd65ee3b80a19
Dawid Drag <dede7d@wp.pl>
2022-08-17 14:35:45 +0200

    task1 1 done

--- 01/app.js
+++ 01/app.js
@@ -8,8 +8,30 @@ class App extends React.Component {
     render() {
         console.log('render');
 
-        return <h1>{ this.state.counter }</h1>
+        return <h1>{this.state.counter}</h1>
     }
+
+    componentDidMount() {
+        console.log('Zamontowano komponent')
+
+        this.id = setInterval(() => { const { counter } = this.state; this.setState({ counter: counter + 1 }) }, 5000)
+    }
+
+
+    componentDidUpdate() {
+        console.log('Zaktualizowano komponent')
+
+    }
+
+
+    componentWillUnmount() {
+        console.log('Odmontowano komponent')
+        clearInterval(this.id)
+
+
+    }
+
+
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
