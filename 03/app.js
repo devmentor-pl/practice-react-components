99d978a47eb9caddec7e38282c5b53aae4680e59
Katarzyna Panz <katarzyna.panz@gmail.com>
2022-09-06 10:10:04 +0200

    Add solution to exercise 03

--- 03/app.js
+++ 03/app.js
@@ -3,22 +3,46 @@ import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
     state = {
+        content: '',
         comments: [],
+        
     }
-    
+
+    clickHandler = event => {
+        console.log('click!', this);
+    }
+
+    renderCommentsList() {
+        const {comments} = this.state;
+        return comments.map(comment => {
+            return(
+                <li onClick={this.clickHandler}>{comment}</li>
+            );
+        });
+    }
+
+    changeHandler = e => {
+        this.setState({
+            content: e.target.value,
+        });
+    }
+
     render() {
         const {title, body} = this.props;
+        const {content} = this.state;
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
                                 <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
                                     name="content" 
+                                    value={content}
+                                    onChange={this.changeHandler}
                                 />
                             </label>
                         </div>
@@ -26,12 +50,31 @@ class Article extends React.Component {
                     </form>
                     <ul>
                         {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
         )
     }
+
+
+submitHandler = e => {
+    e.preventDefault();
+    const {content} = this.state;
+    this.addComment(`${content}`);
+    this.setState({
+        content: '',
+    })
 }
+
+addComment(comment) {
+    this.setState({
+        comments: [...this.state.comments, comment],
+    })
+}
+
+}
+
 
 ReactDOM.render(
     <Article 
