5b1c12b3ed6bd3b7214c381a35c0812bbaeaea4e
Lukasz Szarkowski <lukas.szarkowski@gmail.com>
2022-09-20 16:55:33 +0200

    finish 03

--- 03/app.js
+++ 03/app.js
@@ -2,41 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Article extends React.Component {
-    state = {
-        comments: [],
-    }
-    
-    render() {
-        const {title, body} = this.props;
-        return (
-            <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-                <section>
-                    <form>
-                        <div>
-                            <label>
-                                <textarea 
-                                    style={{ "min-width": "300px", "min-height": "120px" }} 
-                                    name="content" 
-                                />
-                            </label>
-                        </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
-                    </form>
-                    <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
-                    </ul>
-                </section>
-            </article>
-        )
-    }
+	state = {
+		comment: '',
+		comments: [],
+	};
+
+	render() {
+		const { title, body } = this.props;
+		const { comment } = this.state;
+		return (
+			<article>
+				<h1>{title}</h1>
+				<p>{body}</p>
+				<section>
+					<form onSubmit={this.submitHandler}>
+						<div>
+							<label>
+								<textarea style={{ 'minWidth': '300px', 'minHeight': '120px' }} name='content' value={comment} onChange = {this.changeHandler} />
+							</label>
+						</div>
+						<div>
+							<input type='submit' value='dodaj komentarz' />
+						</div>
+					</form>
+					<ul>{this.renderComments()}</ul>
+				</section>
+			</article>
+		);
+	}
+
+	renderComments() {
+		const { comments } = this.state;
+		return comments.map(item => {
+			return <li>{item}</li>;
+		});
+	}
+
+	changeHandler = e => {
+		this.setState({ comment: e.target.value });
+	};
+
+	submitHandler = e => {
+		e.preventDefault();
+		const { comment } = this.state;
+		if (comment.length > 5) {
+            this.addComment(`${comment}`);
+		} else {alert('Comment must have at least 5 characters!')}
+	};
+
+	addComment = item => {
+		this.setState({ comments: [...this.state.comments, item], comment: '' });
+	};
 }
 
 ReactDOM.render(
-    <Article 
-        title="Programowanie jest super!"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />, 
-    document.querySelector('#root')
+	<Article
+		title='Programowanie jest super!'
+		body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam...'
+	/>,
+	document.querySelector('#root')
 );
