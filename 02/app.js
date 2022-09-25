cbf0f5783bea54422ee165acaf0ded2661c6cf67
Szymon Ludwikowski <ludwikowski.szym1993@gmail.com>
2022-09-25 16:20:47 +0200

    Finish 02

--- 02/app.js
+++ 02/app.js
@@ -5,9 +5,14 @@ class Counter extends React.Component {
     state = {
         amount: 0,
     }
-    
+    handleClick(){
+        this.setState({
+            amount: this.state.amount +1,
+
+        })
+    }
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return <button onClick={()=> this.handleClick()}>click me ({ this.state.amount })</button>
     }
 }
 
