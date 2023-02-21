a4b3b4f6f8b04154fd2596f28bfb991e8b4c4c0b
Anna <aniamatrackasadurek@gmail.com>
2023-02-21 15:12:11 +0100

    finish 03

--- 03/app.js
+++ 03/app.js
@@ -5,33 +5,56 @@ const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
     state = {
-        comments: [],
+        text: '',
+        comments: [],     
     }
     
     render() {
         const {title, body} = this.props;
+        const {text} = this.state;
         return (
             <article>
                 <h1>{ title }</h1>
                 <p>{ body }</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
                                 <textarea 
                                     style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                    name="content" value={text} onChange={this.inputChange}
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
         )
+    }
+    renderCommentsList() {
+        const {comments} = this.state;
+        return comments.map(element => {
+            return (
+                <li>{element}</li>
+            )
+        })
+    }
+    submitHandler = e => {
+        e.preventDefault();
+        const {text} = this.state;
+        if(text.length !== 0) {
+            this.addComment(`${text}`);
+        }   else {alert('This comment cannot be added.')}
+    }
+    addComment = text => {
+        this.setState({comments: [...this.state.comments, text], text: ''});
+    }
+    inputChange = e => {
+        this.setState({text: e.target.value})
     }
 }
 
