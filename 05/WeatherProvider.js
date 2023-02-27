9f7f1a3ca2b6f52bf7d232c4ad58c193845b77ff
Mateusz Matwiejczuk <matmatwiej@gmail.com>
2023-02-27 17:42:48 +0100

    finish task 5

---
+++ 05/WeatherProvider.js
@@ -0,0 +1,57 @@
+﻿import React, { Component } from 'react';
+
+export default class Weather extends Component {
+	state = {
+		data: null,
+	};
+
+	render() {
+		const { lat, lng } = this.props;
+		const { data } = this.state;
+
+		if (data) {
+			return (
+				<section>
+					<p>
+						Pogodę w obecnej chwili w miejscu o szerokości geograficznej:
+						<strong>{lat}</strong> i długości geograficznej:
+						<strong>{lng}</strong> <strong>({data.city})</strong> można określić
+						jako: <strong>{data.desc}</strong>, gdzie temperatura wynosi:{' '}
+						<strong>{data.temp}</strong>&#8457;
+					</p>
+				</section>
+			);
+		} else {
+			return null;
+		}
+	}
+
+	componentDidMount() {
+		this.getWeatherData();
+	}
+
+	getWeatherData() {
+		const { lat, lng } = this.props;
+		const API_KEY = 'f93c6d18427e4c79aca3fb72a6ad546f';
+		const URL = `https://api.weatherbit.io/v2.0/current?key=${API_KEY}&lat=${lat}&lon=${lng}&units=I&lang=pl`;
+
+		const promise = fetch(URL);
+		promise
+			.then((resp) => {
+				if (resp.ok) {
+					return resp.json();
+				}
+				throw new Error('Brak danych!');
+			})
+			.then((data) => {
+				const copyData = Object.assign({}, ...data.data);
+				this.setState({
+					data: {
+						temp: copyData.temp,
+						desc: copyData.weather.description,
+						city: copyData.city_name,
+					},
+				});
+			});
+	}
+}
