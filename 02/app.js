ceb96bdcc3b94c69fda47c3734c0df56d1d3a757
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-03-06 22:48:59 +0100

    02 done

--- 02/app.js
+++ 02/app.js
@@ -4,13 +4,44 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Counter extends React.Component {
-    state = {
-        amount: 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            amount: 0,
+        }
     }
-    
+    handleClick = () => {
+        this.setState({ amount: this.state.amount + 1 })
+    }
+
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.amount}
+            </button>
+        )
+    }
+    componentDidMount() {
+        const { amount } = this.state;
+
+        const newState = {
+            amount: amount + 1
+        }
+
+        this.setState(newState)
+
     }
 }
 
 root.render(<Counter />);
+
+// componentDidMount() {
+//     const newState = (state, props) => {
+//     const { counter } = state;
+//     const { step } = props;
+//     return {
+//     counter: counter + step,
+//     }
+//     }
+//     this.setState(newState);
+//     }
