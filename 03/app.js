eaed096bb2deef4f69e9c5fa0abcaf127d1d46f8
Miłosz Szmulewicz <miloszszmulewicz@gmail.com>
2023-03-07 15:35:59 +0100

    03 done

--- 03/app.js
+++ 03/app.js
@@ -4,39 +4,72 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
-    state = {
-        comments: [],
+    constructor() {
+        super();
+        this.state = {
+            comments: ["jwkdbv", "kjsbc"],
+            textarea: '',
+        }
     }
-    
+    renderCommentsList() {
+        const { comments } = this.state;
+        return comments.map(text => {
+            return (
+                <li onClick={this.clickHandler}>{text}</li>
+            );
+        });
+    }
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
+        const { textarea } = this.state;
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }}
+                                    name="content"
+                                    value={textarea}
+                                    onChange={this.changeHandler}
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
         )
     }
+    changeHandler = e => {
+        const { tagName, value } = e.target;
+        this.setState({
+            [tagName.toLowerCase()]: value,
+        });
+    };
+    submitHandler = e => {
+        e.preventDefault();
+        const { textarea } = this.state;
+        this.addComments(textarea);
+        this.setState({
+            textarea: '',
+        });
+    }
+    addComments(text) {
+        this.setState({
+            comments: [...this.state.comments, text]
+        });
+    }
 }
 
 root.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
     />
