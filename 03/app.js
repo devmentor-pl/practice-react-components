79276c93c097aff2fdf61fafb15eac62ed98f611
Michal <maxpoz3212@gmail.com>
2023-03-21 18:43:30 +0100

    Add task 03 solution

--- 03/app.js
+++ 03/app.js
@@ -5,38 +5,87 @@ const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
     state = {
+        textareaContent: '',
         comments: [],
     }
-    
+
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
+        const { textareaContent, comments } = this.state;
+
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    value={textareaContent}
+                                    onChange={this.textareaChange}
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }}
+                                    name="textareaContent"
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
         )
     }
+
+    textareaChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        const { textareaContent } = this.state;
+
+        if (textareaContent) {
+            this.addComment(textareaContent);
+            this.clearTextarea();
+        } else {
+            alert('Textarea is empty!');
+        }
+    }
+
+    clearTextarea() {
+        this.setState({
+            textareaContent: '',
+        });
+    }
+
+    addComment(newComment) {
+        const { comments } = this.state;
+
+        this.setState({
+            comments: [...comments, newComment]
+        });
+    }
+
+    renderCommentsList() {
+        const { comments } = this.state;
+
+        return comments.map(comment => {
+            return (
+                <li key={comment}>{comment}</li>
+            )
+        });
+    }
 }
 
 root.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
     />
