05fa5847f39b8c37d9f134ca0d8255e3f13f1b4b
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-06-12 00:00:57 +0200

    finish task1

--- 01/app.js
+++ 01/app.js
@@ -4,14 +4,35 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        }
     }
+    
+
     render() {
         console.log('render');
 
         return <h1>{ this.state.counter }</h1>
     }
+
+    componentDidMount() {
+        this.id = setInterval(()=> {
+            const {counter} = this.state;
+            this.setState({counter:counter+1})
+        }, 5000)
+    }
+
+    componentDidUpdate() {
+        console.log("update!")
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.id);
+    }
+
 }
 
 root.render(<App/>);
