e121db873dd80acba490e303806363e1b1a6a150
Jędrzej Kmak <jedrzej.kmak@gmail.com>
2023-07-10 20:42:30 +0200

    refactor api

--- 05/app.js
+++ 05/app.js
@@ -1,44 +1,20 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
+import Weather from "./Weather";
 
 const root = createRoot(document.querySelector('#root'))
 
-class Weather extends React.Component {
-    state = {
-        data: null,
-        key: '3be1579ab3be42d78e26bfae3f3b9db8',
-    }
-
+class App extends React.Component {
     render() {
-        const {data} =  this.state;
-        if (data) {
-            return (
-                <section>
-                    <h2>{data.city_name}</h2>
-                    <h2>{data.temp} C</h2>
-                    <h2>{data.weather.description}</h2>
-                </section>
-            )
-        }
-        return null;
-    }
-
-    componentDidMount() {
-        this.getWeatherData();
-    }
-
-    async getWeatherData () {
-        const {lat, lng} = this.props;
-        const resp =  await fetch(`https://api.weatherbit.io/v2.0/current?key=${this.state.key}&lat=${lat}&lon=${lng}&lang=pl&units=M`);
-        const weatherData = await resp.json();
-        this.setState({
-            data: weatherData.data[0],
-        })
+        return (
+            <section>
+                <Weather
+                    lat={52.232222} 
+                    lng={21.008333}
+                />
+            </section>
+        )
     }
 }
 
-root.render(
-    <Weather
-        lat={52.232222} 
-        lng={21.008333}
-    />)
+root.render(<App />)
