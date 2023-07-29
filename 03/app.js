51a000fdd03934e4668e0ddd822337e6f99c1af8
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-07-29 13:24:03 +0200

    Finish task 03

--- 03/app.js
+++ 03/app.js
@@ -5,38 +5,62 @@ const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
     state = {
+        comment: '',
         comments: [],
     }
-    
+
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
+        const { comment } = this.state
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    onChange={this.changeHandler}
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }}
+                                    name="content"
+                                    value={comment}
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.renderCommentsList()}
                     </ul>
                 </section>
             </article>
         )
     }
+
+    changeHandler = (e) => {
+        const { comment } = this.state  // trochę nie rozumiem dlaczego comment jest wyszarzony, jeśli zakomentuje tą linijkę, comment jest cały czas zdefiniowany- czy chodzi o closures? Jeśli tak, to wcześniej rozumiałam to tak, że mimo wszytsko zmienna w pierwszej kolejności pobierana jest z najbliższego scope, czyli z tej bieżącej linii kodu 42. 
+
+        this.setState({ comment: e.target.value })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+
+        const { comment, comments } = this.state
+
+        this.setState({comments:[...comments, comment]})
+        this.setState({comment: ''})
+    }
+
+    renderCommentsList() {
+        const { comments } = this.state
+        return comments.map((item, index) => <li key={index}>{item}</li>)
+    }
 }
 
 root.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
     />
