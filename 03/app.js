7ac9bbec4129d9c8371878c46588d753d8668a2a
Marcin <xcinek1337@wp.pl>
2023-08-29 08:02:05 +0200

    03

--- 03/app.js
+++ 03/app.js
@@ -4,40 +4,48 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
-    state = {
-        comments: [],
-    }
-    
-    render() {
-        const {title, body} = this.props;
-        return (
-            <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
-                <section>
-                    <form>
-                        <div>
-                            <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
-                                />
-                            </label>
-                        </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
-                    </form>
-                    <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
-                    </ul>
-                </section>
-            </article>
-        )
-    }
+	state = {
+		textarea: '',
+		comments: [],
+	};
+
+	render() {
+		const { title, body } = this.props;
+		return (
+			<article>
+				<h1>{title}</h1>
+				<p>{body}</p>
+				<section>
+					<form onSubmit={this.submitHandler}>
+						<div>
+							<label>
+								<textarea style={{ minWidth: '300px', minHeight: '120px' }} name='content' />
+							</label>
+						</div>
+						<div>
+							<input type='submit' value='dodaj komentarz' />
+						</div>
+					</form>
+					<ul>{/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}</ul>
+				</section>
+			</article>
+		);
+	}
+	submitHandler = e => {
+		e.preventDefault();
+	
+		// this.addComment({  });
+	};
+	addComment(com) {
+		this.setState({
+			comments: [...this.state.comments, com],
+		});
+	}
 }
 
 root.render(
-    <Article 
-        title="Programowanie jest super!"
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
-    />
+	<Article
+		title='Programowanie jest super!'
+		body='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam...'
+	/>
 );
