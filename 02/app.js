1434bbe629e445189eb3f1d509afa2de33fd98bb
Marcin Soltysik <profittax@gmail.com>
2023-08-31 22:32:55 +0200

    React-e2-Done 02

--- 02/app.js
+++ 02/app.js
@@ -4,13 +4,23 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class Counter extends React.Component {
-    state = {
-        amount: 0,
-    }
-    
-    render() {
-        return <button>click me ({ this.state.amount })</button>
-    }
+  state = {
+    amount: 0,
+  };
+
+  handlClick() {
+    this.setState({
+      amount: this.state.amount + 1,
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={() => this.handlClick()}>
+        click me ({this.state.amount})
+      </button>
+    );
+  }
 }
 
 root.render(<Counter />);
