abef2a97b1f11274b28807a1859a2cf0e0eb3cc0
Marcin Soltysik <profittax@gmail.com>
2023-09-01 00:20:51 +0200

    React-e2-Done 05

---
+++ 05/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Weather from './Weather';
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(<Weather lat={52.232222} lng={21.008333} />);
