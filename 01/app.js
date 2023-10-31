045efbe07deaefa86f173055cd969fa372955c8d
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-10-31 22:34:17 +0100

    task 1 done

--- 01/app.js
+++ 01/app.js
@@ -4,13 +4,24 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 }
     }
     render() {
-        console.log('render');
-
         return <h1>{ this.state.counter }</h1>
+    }
+    componentDidMount() {
+        this.id = setInterval(() => {
+            const { counter } = this.state;
+            this.setState( {counter: counter + 1} )
+        }, 5000)
+    }
+    componentDidUpdate() {
+        console.log('Komunikat po aktualizacji');
+    }
+    componentWillUnmount() {
+        clearInterval( this.id );
     }
 }
 
