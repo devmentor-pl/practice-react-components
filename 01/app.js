ea3d9dce0a4843eb1e843a3ffb691df116239f41
Michal Gawlik <gawlik.zator@o2.pl>
2023-12-22 18:05:31 +0100

    task 01 complete

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector('#root'))
 
 class App extends React.Component {
-    state = {
-        counter: 0,
-    }
-    render() {
-        console.log('render');
+	constructor(props) {
+		super(props)
+		this.state = {
+			counter: 0,
+		}
+	}
 
-        return <h1>{ this.state.counter }</h1>
-    }
+	render() {
+		console.log('render')
+
+		return <h1>{this.state.counter}</h1>
+	}
+
+	componentDidMount() {
+		console.log('componentDidMount')
+		this.id = setInterval(() => {
+			const { counter } = this.state
+			const newState = { counter: counter + 1 }
+			this.setState(newState)
+		}, 5000)
+	}
+
+	componentDidUpdate() {
+		console.log('componentDidUpdate')
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.id)
+		console.log('componentWillUnmount')
+	}
 }
 
-root.render(<App/>);
+root.render(<App />)
