19d8ed65e245657d01f357896b97cdc89b8e4e83
Piotr Marczykowski <piotr.marczykowski94@gmail.com>
2024-01-23 19:53:45 +0100

    add task 05

--- 05/app.js
+++ 05/app.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   componentDidMount() {
     const { lat, lng } = this.props;
 
-    const apiKey = "7d52688e95b94d259f3f5de564f5362c";
+    const apiKey = "90567dc98b824867acdeb4bcb2c51964";
 
  const apiUrl = `https://api.weatherbit.io/v2.0/current?key=${apiKey}&lat=${lat}&lon=${lng}&lang=pl`;
 //  const apiUrl = `https://api.weatherbit.io/v2.0/current?lat=52.232222&lon=21.008333&key=7d52688e95b94d259f3f5de564f5362c`;
@@ -32,7 +32,7 @@ class App extends React.Component {
   render() {
     const { data } = this.state;
   
-    if (data && data.data && data.data.length === 0) {
+    if (data && data.data && data.data.length > 0) {
       const weatherInfo = data.data[0];
   
       return (
