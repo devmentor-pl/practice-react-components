7a58b57238cdf8dedd031c8e2f9b52a91cb10a03
Dawidtk <dawidtk@gmail.com>
2024-02-05 08:20:27 +0100

    App

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-const root = createRoot(document.querySelector('#root'));
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
     state = {
         counter: 0,
-    }
-    render() {
-        console.log('render');
-
-        return <h1>{ this.state.counter }</h1>
-    }
-}
-
-root.render(<App/>);
+    };
