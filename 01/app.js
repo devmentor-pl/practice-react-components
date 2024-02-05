d7500fd02f64c7a013d6b70427dc21dcf75513d3
Dawidtk <dawidtk@gmail.com>
2024-02-05 08:26:15 +0100

    update

--- 01/app.js
+++ 01/app.js
@@ -5,3 +5,19 @@ class App extends React.Component {
     state = {
         counter: 0,
     };
+
+    componentDidMount() {
+        this.stopInterval = setInterval(() => {
+            const { counter } = this.state;
+            this.setState({ counter: counter + 1 }, () => {
+                console.log('Callback');
+            });
+        }, 1000);
+        console.log('Component DidMount');
+    }
+
+    componentDidUpdate() {
+        console.log('Component DidUpdate');
+    }
+
+    
