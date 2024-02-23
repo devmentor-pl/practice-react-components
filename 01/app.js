eacefae0a0d79ed7a67d324047042696fffe0e7b
Marcin Chechelski <marcin.chechelski@op.pl>
2024-02-23 17:47:17 +0100

    task 01 done

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.querySelector('#root'));
+const root = createRoot(document.querySelector("#root"));
 
 class App extends React.Component {
     state = {
         counter: 0,
-    }
-    render() {
-        console.log('render');
+    };
 
-        return <h1>{ this.state.counter }</h1>
+    render() {
+        console.log("render");
+
+        return <h1>{this.state.counter}</h1>;
+    }
+
+    constructor(props) {
+        super(props);
+        console.log("constructor");
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.id = setInterval(() => {
+            this.setState({
+                counter: this.state.counter + 1,
+            });
+        }, 5000);
+    }
+
+    componentDidUpdate() {
+        console.log("componentDidUpdate");
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+        clearInterval(this.id);
     }
 }
 
-root.render(<App/>);
+root.render(<App />);
